b3e620eea5d223ff535ad6c094827f6e32e4a5c9
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-06-10 12:31:58 +0530

    added dummy data

--- src/DataForPage/dummyData.jsx
+++ src/DataForPage/dummyData.jsx
@@ -79,3 +79,22 @@ export const FooterData2 = [
     text: "Cookies",
   },
 ];
+
+export const FooterData3 = [
+  {
+    id: "1",
+    text: "Support",
+  },
+  {
+    id: "2",
+    text: "Customer support",
+  },
+  {
+    id: "3",
+    text: "Media",
+  },
+  {
+    id: "4",
+    text: "Marketing",
+  },
+];
