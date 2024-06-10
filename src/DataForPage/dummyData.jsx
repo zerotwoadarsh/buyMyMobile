b2caf6e04b72f07f8edb177af4178ea7f073e5bb
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-06-10 12:30:19 +0530

    added dummy data

---
+++ src/DataForPage/dummyData.jsx
@@ -0,0 +1,38 @@
+import chair from "../assets/images/chairPurple.png";
+import sofa2 from "../assets/images/roseChair.png";
+import sofa3 from "../assets/images/gray-chair.png";
+import phone1 from "../assets/images/bedRoom.png";
+import blueSofa from "../assets/images/blueSofa.png";
+
+export const navbarData = [
+  {
+    id: "1",
+    name: "News",
+    link: "news",
+  },
+  {
+    id: "2",
+    name: "Company",
+    link: "company",
+  },
+  {
+    id: "3",
+    name: "Features",
+    link: "features",
+  },
+  {
+    id: "4",
+    name: "Room",
+    link: "room",
+  },
+  {
+    id: "5",
+    name: "Demo",
+    link: "demo",
+  },
+  {
+    id: "6",
+    name: "Login",
+    link: "/",
+  },
+];
